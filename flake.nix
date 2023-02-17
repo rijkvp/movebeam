bcfb639eb3d404f7a80cc9b2851f6fc3c3db7a62
rijkvp <rijk@rijkvp.nl>
2023-02-17 15:22:28 +0100

    Add nix build

--- flake.nix
+++ flake.nix
@@ -1,25 +1,24 @@
 {
-  description = "Rust devShell";
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+        inherit (nixpkgs) lib;
+        pkgs = nixpkgs.legacyPackages.${system};
       in
       with pkgs;
       {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "movebeam";
+          inherit ((lib.importTOML (self + "/Cargo.toml")).package) version;
+          src = self;
+          cargoLock.lockFile = self + "/Cargo.lock";
+        };
         devShells.default = mkShell {
-          buildInputs = [
-            pkg-config
-            rust-bin.stable.latest.default
-          ];
+          nativeBuildInputs = with pkgs; [ rustc cargo ];
         };
       }
     );
